9b414e825b0983635db17f0a04bec177ad96bc30
sqram <bruce83@gmail.com>
2020-03-01 20:47:58 -0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+   <p>Home page </p>
+   <p>Accessed via '/home'</p>
+   <p>Works well with `generate` - creates `/home/index.html` instead of `/index.html`</p>
+  </div>
+</template>
+
+
+<router>
+  {
+    path: '/home'
+  }
+</router>
+
+
+
+
---
+++ pages/products/chairs/_.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>    
+    <h2>Chair type: <u>{{ id }}</u></h2>    
+
+    <br>br> 
+
+    Desired generated file would be, ie,  `/products/bench/index.html`
+    <br> <br>
+    
+    Instead it's `/products/chairs/bench/index.html`
+  </div>
+</template>
+
+
+<router>
+  {
+    path: '/products/:id',    
+  }
+</router>
+
+
+<script>
+
+
+export default {  
+  asyncData({ params }) {    
+    return { id: params.id}
+  },
+
+  data () {
+    return {
+      id: ''
+    }
+  }
+}
+</script>
